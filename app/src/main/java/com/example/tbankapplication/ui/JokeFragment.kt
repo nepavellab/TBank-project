25c27ceaebf582477c3dff711fe069f0b7138fb2
Pavel N <trudi2004@yandex.ru>
2024-11-12 00:32:23 +0300

    Приложение полностью переведено на фрагменты

---
+++ app/src/main/java/com/example/tbankapplication/ui/JokeFragment.kt
@@ -0,0 +1,43 @@
+package com.example.tbankapplication.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tbankapplication.data.Data.jokes
+import com.example.tbankapplication.databinding.JokeFragmentBinding
+
+class JokeFragment : Fragment() {
+    private lateinit var binding: JokeFragmentBinding
+    companion object {
+        private const val KEY = "POSITION"
+
+        fun newInstance(position: Int): JokeFragment {
+            return JokeFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(KEY, position)
+                }
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = JokeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.run {
+            val index = getInt(KEY)
+            binding.tvJokeCategory.text = jokes[index].category
+            binding.tvQuestion.text = jokes[index].question
+            binding.tvAnswer.text = jokes[index].answer
+        }
+    }
+}
